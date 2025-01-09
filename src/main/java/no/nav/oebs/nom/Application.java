ccd64f4f64a6c02e52a4b74ffff02d6cd5666ceb
BougrNAV <Abid.Bougroug@nav.no>
2025-01-09 13:02:30 +0100

    Endret logging

--- src/main/java/no/nav/oebs/nom/Application.java
+++ src/main/java/no/nav/oebs/nom/Application.java
@@ -1,5 +1,10 @@
 package no.nav.oebs.nom;
 
+import no.nav.oebs.nom.config.PropertySourceLogger;
+import no.nav.oebs.nom.db.repository.NomsLoggRepositoryCustom;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +14,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class Application {
+
+	private final Logger logger = LoggerFactory.getLogger(Application.class);
 
 	public static void main(String[] args) {
 
